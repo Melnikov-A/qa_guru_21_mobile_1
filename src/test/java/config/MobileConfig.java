e23b149f2b87eda71bf40fc2862014f6d28bfd13
Anton Melnikov <101597291+Melnikov-A@users.noreply.github.com>
2023-11-21 15:31:26 +0300

    add project

---
+++ src/test/java/config/MobileConfig.java
@@ -0,0 +1,32 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+
+@Config.Sources({
+        "classpath:${env}.properties",
+        "classpath:android.properties"
+})
+public interface MobileConfig extends Config {
+
+    @Key("platformName")
+    String getPlatformName();
+
+    @Key("deviceName")
+    String getDeviceName();
+
+    @Key("osVersion")
+    String getVersion();
+
+    @Key("app")
+    String getApp();
+
+    @Key("projectName")
+    String getProject();
+
+    @Key("buildName")
+    String getBuild();
+
+    @Key("testName")
+    String getTestName();
+}
